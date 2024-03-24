9975b4abbb4af5cf49e1c95f3c3c7c1846ca6f77
NelcidoDiaz <60740045+nelrafa103@users.noreply.github.com>
2024-03-23 22:13:50 -0400

    Adding i18n and new ilustrations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,37 @@
+export const languages = {
+	en: 'English',
+	es: 'Spanish',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+	en: {
+		'nav.about': 'About',
+		'nav.projects': 'Projects',
+		'nav.contact': 'Contact',
+		'nav.download': 'Download CV',
+		'hero.greetings': 'Hi! My name is ',
+		'hero.name': 'Nelcido Diaz',
+		'hero.tittle': " and I'am a web developer based in Dominican Republic.",
+		'hero.learn': 'Learn more',
+		'about.tittle': 'About me',
+		'about.content':
+			"I am a 22-year-old developer who loves learning and playing video games. I stay up-to-date in my field and enjoy the satisfaction of solving problems and creating new things. When I'm not working, I relax by playing video games, an activity that allows me to explore virtual worlds and challenge my skills. In short, I am a young professional with a curious mind and an adventurous spirit.",
+		'contact.button': 'Say hello!',
+	},
+	es: {
+		'nav.about': 'Acerca de mi',
+		'nav.projects': 'Projectos',
+		'nav.contact': 'Contacto',
+		'nav.download': 'Descargar CV',
+		'hero.greetings': 'Hola! Mi nombre es ',
+		'hero.name': 'Nelcido Diaz',
+		'hero.tittle': ' y soy un desarrollador web con residencia en Republica Dominicana.',
+		'hero.learn': 'Saber más',
+		'about.tittle': 'Acerca de mi',
+		'contact.button': 'Di hola!',
+		'about.content':
+			'Soy un desarrollador de 22 años apasionado por el aprendizaje y los videojuegos. Me mantengo actualizado en mi campo y disfruto de la satisfacción de resolver problemas y crear cosas nuevas. Cuando no estoy trabajando, me relajo jugando videojuegos, una actividad que me permite explorar mundos virtuales y desafiar tus habilidades. En resumen,un joven profesional con una mente curiosa y un espíritu aventurero.',
+	},
+} as const;
